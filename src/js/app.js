59ee869344097dde8509c5dfaabc11f3c3feeb50
YarikSintsov <sintsovyarik@yandex.ru>
2024-10-05 17:10:23 +0300

    first commit

---
+++ src/js/app.js
@@ -0,0 +1,51 @@
+// Импорт bootstrap
+// import "../../node_modules/bootstrap/dist/js/bootstrap.bundle.js"
+
+// import {isWebp} from './includes/functions/isWebp.js'
+// import {menuInit} from './includes/functions/burgerMenu.js'
+// import {tabs} from './includes/functions/tabs.js'
+// import {spollers} from './includes/functions/spollers.js'
+// import * as scroll from './includes/functions/scroll/scroll.js'
+// import * as forms from './includes/functions/forms/forms.js'
+
+//============================================================
+// Проверка поддержки webp
+// isWebp()
+
+// Меню бургер
+// menuInit()
+
+// Табы (вкладки)
+// tabs()
+
+// Модуль работы со спойлерами
+// spollers()
+
+//============================================================
+// Плавная навигация по странице
+// scroll.pageNavigation()
+
+// Функционал добавления классов к шапке при прокрутке
+// scroll.headerScroll()
+
+// Функционал липкого блока
+// scroll.stickyBlock()
+
+//============================================================
+// Слайдер swiper
+// import './includes/functions/sliders.js'
+
+//============================================================
+// Динамический адаптив
+// import './includes/libs/dynamicAdapt.js'
+
+//============================================================
+// Попапы
+// import './includes/libs/popup.js'
+
+//============================================================
+// Модуль работы с select
+// import './includes/libs/select.js'
+
+// Модуль звездного рейтинга
+// forms.formRating()
